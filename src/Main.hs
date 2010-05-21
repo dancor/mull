52822b458f3ddb4db590592e626fef89d4569fae
Daniel Corson <danl@alum.mit.edu>
2010-05-20 22:36:53 -0400

    switch to polyopt (no config file)

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,18 @@
 module Main where
 
-import Ask
 import Control.Monad
 import Control.Monad.Random
 import Data.Either
 import Data.Maybe
 import FUtil
-import Math
-import Mus
 import System.Console.GetOpt
 import System.Environment
 import System.Random
 
-data Options = Options {
-  oNum :: Maybe Int}
-
-options :: [OptDescr (Options -> Options)]
-options = [
-  Option "n" ["ask-num"]
-    (ReqArg (\ n o -> o {oNum = Just $ read n}) "NUM") "only ask NUM questions"
-  ]
-
-defaultOptions :: Options
-defaultOptions = Options {
-  oNum = Nothing}
-
-askRand :: (MonadRandom r) => [r a] -> r a
-askRand fs = choice fs >>= id
+import Ask
+import Math
+import Mus
+import Opt
 
 evalRandTIO :: RandT StdGen IO a -> IO a
 evalRandTIO f = do
@@ -36,7 +22,6 @@ evalRandTIO f = do
 
 main :: IO ()
 main = do
-  args <- getArgs
   let
     subjects :: [AskDesc StdGen]
     subjects = [
@@ -47,15 +32,13 @@ main = do
       mul23,
       elop24,
       elop30]
-    usage = "usage: mull [options] <subjects>"
-    subjMsg = unlines $
-      "subjects are:":map (\ (Ask s d _) -> s ++ "\t" ++ d) subjects
-    doErr e = error $ e ++ usageInfo usage options ++ subjMsg
-    (opts, moreArgs) = case getOpt Permute options args of
-      (o, n, []) -> (foldl (flip id) defaultOptions o, n)
-      (_, _, errs) -> doErr $ concat errs
+    usage = "usage: mull [options] <subjects>\n" ++ Opt.optInfo ++ "\n" ++
+      unlines ("subjects are:":map (\ (Ask s d _) -> s ++ "\t" ++ d) subjects)
+  (opts, args) <- Opt.getOpts "" usage
+  let
+    doErr e = error $ e ++ usage
     subjs :: [AskDesc StdGen]
-    subjs = case moreArgs of
+    subjs = case args of
       [] -> doErr ""
       ss -> case unfound of
           [] -> found
@@ -64,6 +47,6 @@ main = do
         (unfound, found) = partitionEithers $
           map (\ s -> maybe (Left s) Right . listToMaybe $
             filter (\ (Ask s' _ _) -> s' == s) subjects) ss
-    askNF = maybe id take $ oNum opts
-  sequence_ . askNF . repeat . evalRandTIO . askRand $ map (\ (Ask _ _ r) -> r)
-    subjs
+    askNFunc = maybe id take $ Opt.askNum opts
+  sequence_ . askNFunc . repeat . evalRandTIO . join . choice $
+    map (\ (Ask _ _ r) -> r) subjs
